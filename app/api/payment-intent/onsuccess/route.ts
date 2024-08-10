735d787221e158ea8a05810789f41e69307a63dd
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-08-10 14:14:48 +0100

    Update route.ts

--- app/api/payment-intent/onsuccess/route.ts
+++ app/api/payment-intent/onsuccess/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 
 export async function GET(req:Request){
-    const data = await req.json
+    const data = await req.json()
 
     console.log("data here", data)
     return new NextResponse("respond from the webhook later here",{status:200} )
